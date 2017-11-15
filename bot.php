4aecf86b6cd7b4c7a3f5cde9f8585f67fd796338
s5801012630092 <31880220+s5801012630092@users.noreply.github.com>
2017-11-15 19:12:59 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -120,7 +120,7 @@ if (!is_null($events['events'])) {
 					];
 					$me = [
 						'type' => 'text',
-						'text' => 'ถ้าไม่มีลองหาที่ซื้อร่ม โดยพิมพ์คำว่า ห้างสรรพสินค้า ดูสิ'
+						'text' => 'ถ้าไม่มีลองหาที่ซื้อร่ม โดยพิมพ์คำว่า ร้านค้า ดูสิ'
 					];
 					// Make a POST Request to Messaging API to reply to sender
 					$url = 'https://api.line.me/v2/bot/message/reply';
