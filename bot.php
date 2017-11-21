eeef24d7e4d60ea088fe845de5e57eb8efeeb048
s5801012630092 <31880220+s5801012630092@users.noreply.github.com>
2017-11-22 02:06:49 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -85,7 +85,7 @@ if (!is_null($events['events'])) {
 					];
 					$mess = [
 						'type' => 'text',
-						'text' => 'โดยพิมพ์คำว่า ร้านอาหาร ดูนะ'
+						'text' => 'โดยพิมพ์คำว่า ร้านอาหาร หรือกดที่ รูปร้านอาหาร ก็ได้นะ'
 					];
 					// Make a POST Request to Messaging API to reply to sender
 					$url = 'https://api.line.me/v2/bot/message/reply';
