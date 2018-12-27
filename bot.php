7f2d17b18121d7e34edcaf7199f9411a1d9a089b
Ittirit Thipphinit <itchampclub@gmail.com>
2018-12-27 14:40:43 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -674,22 +674,16 @@ if (!is_null($events['events'])) {
 				'messages' => [$messages]
 			];
 		}
-		error_log(json_encode($data));
-		$post = json_encode($data);
-		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_POST, true);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			
-		$result = curl_exec($ch);
-		curl_close($ch);
-		url_close($ch);
-		error_log($result);
-		echo $result . "\r\n";		
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);	
 	}
 }
 echo "OK";
