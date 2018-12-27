eeeb54320d0a3827d0a8e4aea7c12c916193d96c
Ittirit Thipphinit <itchampclub@gmail.com>
2018-12-27 14:48:35 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -4,7 +4,7 @@
 $access_token = '8LSoXWYVTlV7oV82tKW6Rw9YdFLm/kcM4FzC2LACY+zpCP00zb012tMeG/NakCYCDP/y9aYS5nyZpW9vmFqihBSlQu0wn+fM9Z86qz9atTM6+KFcIZgPglsuGOfMGRaSGZ+Ur9r1DipRHh31MvR/3wdB04t89/1O/w1cDnyilFU=';
 
 
-		$content = file_get_contents('php://input');
+$content = file_get_contents('php://input');
 $jsonString = file_get_contents('php://input');
 error_log($jsonString);
 // Parse JSON
@@ -819,4 +819,32 @@ if (!is_null($events['events'])) {
 			curl_close($ch);	
 	}
 }
+else
+{
+					$messages = [
+						'type' => 'text',
+						'text' => '่ส่งจากบอท'
+					];
+					// Make a POST Request to Messaging API to reply to sender
+					$url = 'https://api.line.me/v2/bot/message/push';
+	                                $id = 'Ub3ea97c513612d6e3401302f051f81dc ';
+					$data = [
+						'to' => $id,
+						'messages' => [$messages]
+					];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+}
+
+
+
+
 echo "OK";
